469c108a1afbb467ec250a7704d0920b9b862a96
lchiesa <lchiesa@factorit.com.ar>
2021-03-31 00:06:45 -0300

    Ajustes para deployment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 //Habilita express.json
 app.use(express.json({extended: true}));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Importar rutas
 app.use('/api/users', require('./routes/users'));
@@ -19,6 +19,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-app.listen(PORT, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`Server running at ${PORT}`)
 });
